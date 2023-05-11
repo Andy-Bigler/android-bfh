6339ba0b7b7992a65aa9a7b92d50489feb1725a6
Bigler Andy <andy.bigler@students.bfh.ch>
2023-05-11 10:02:22 +0200

    Finish JNI app

--- 10_JNI/app/src/main/java/com/example/jni/MainActivity.kt
+++ 10_JNI/app/src/main/java/com/example/jni/MainActivity.kt
@@ -2,24 +2,37 @@ package com.example.jni
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-
-    // Load the native library
     init {
         System.loadLibrary("native-lib")
     }
 
-    external fun encodeBase64(input: String): String
+    private external fun encodeBase64(input: String): String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val originalString = "Hello, World!"
-        val encodedString = encodeBase64(originalString)
+        val editText = findViewById<EditText>(R.id.editText)
+        val button = findViewById<Button>(R.id.button)
+        val textView = findViewById<TextView>(R.id.textView)
 
-        Toast.makeText(this, "Base64: $encodedString", Toast.LENGTH_LONG).show()
+        button.setOnClickListener {
+            val input = editText.text.toString()
+            if (input.isBlank()) {
+                Toast.makeText(this, "Please enter a text", Toast.LENGTH_SHORT).show()
+                textView.text = ""
+                return@setOnClickListener
+            }
+
+            val encoded = encodeBase64(input)
+
+            textView.text = encoded
+        }
     }
 }
